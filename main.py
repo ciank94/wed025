994ce7db533c53abd4d233c5254300b5290072d7
Cian Kelly <ciank@ntnu.no>
2024-10-23 16:09:20 +0200

    new era5 fields downloaded for NEMO;

--- main.py
+++ main.py
@@ -2,53 +2,79 @@ import netCDF4 as nc
 import matplotlib.pyplot as plt
 import cartopy.crs as ccrs
 import numpy as np
-remote_folder = 'D:/nemo_4.2.2/cfgs/ERA5_input/'
-#filename = remote_folder + 'AQUA_MODIS.20170701.L3m.DAY.PAR.par.4km.nc'
-filename = remote_folder + 'AQUA_MODIS.20170201.L3m.DAY.PAR.par.4km.nc'
-df = nc.Dataset(filename)
-par = np.array(df['par'])
-par[par<-10000]=np.nan
-lon = np.array(df['lon'])
-idxlon = (lon<-30)&(lon>-42)
-lon = lon[idxlon]
-lat = np.array(df['lat'])
-idxlat = (lat<-50)&(lat>-58)
-lat = lat[idxlat]
-par1 = par[idxlat,:]
-par2 = par1[:,idxlon]
-par2 = (par2*6.02e23)/(86400*2.77e18)
-fig, axs = plt.subplots(figsize=(12, 8), nrows=1, ncols=1, subplot_kw={'projection': ccrs.PlateCarree()})
-fig1 = axs.pcolormesh(lon, lat, par2, cmap=plt.get_cmap('hot'), transform=ccrs.PlateCarree())
-fig.colorbar(fig1, label='W m${^-2}$', shrink=.5)
-axs.set_title('datetime: ' + df.time_coverage_start)
-axs.gridlines(draw_labels=True, dms=True, x_inline=False, y_inline=False)
-axs.coastlines()
-plt.show()
-breakpoint()
-lon = df['lon']
-lat = df['lat']
-for str_v in ['u10','v10', 'd2m','t2m', 'msl', 'sp', 'tp',  'sf','strd','ssrd']:
-    var = df[str_v]
-    fig, axs = plt.subplots(figsize=(8, 8), nrows=1, ncols=1, subplot_kw={'projection': ccrs.PlateCarree()})
-    fig1 = axs.pcolormesh(lon, lat, var[0, :, :], cmap=plt.get_cmap('hot'),
-                          transform=ccrs.PlateCarree())
-    axs.coastlines()
-    fig.colorbar(fig1, label=var.name + ' units =' + var.units)
-    axs.set_title('long_name = ' + var.long_name)
-    axs.gridlines(draw_labels=True, dms=True, x_inline=False, y_inline=False)
-    #axs.set_extent([0, 100, 0, 100])
-    plt.show()
-breakpoint()
+from datetime import datetime, timedelta
 
 
-
-
-
-
-
-
-remote_folder = 'D:/nemo_4.2.2/cfgs/ERA5_input/'
+remote_folder = 'E:/nemo_4.2.2/cfgs/ERA5_input/'
 filename = remote_folder + 'interp_2009.nc'
+#df = nc.Dataset(filename, mode='a')
+df = nc.Dataset(filename)
+lon = df['lon']
+lat = df['lat']
+#list1 = ['u10','v10', 'd2m','t2m', 'msl', 'sp', 'tp',  'sf','strd','ssrd']
+list2 = ['mtpr',  'msdwswrf','msdwlwrf','msr']
+for str_v in list2:
+    var = df[str_v]
+    fig, axs = plt.subplots(figsize=(8, 8), nrows=1, ncols=1, subplot_kw={'projection': ccrs.PlateCarree()})
+    fig1 = axs.pcolormesh(lon, lat, var[200, :, :], cmap=plt.get_cmap('hot'),
+                          transform=ccrs.PlateCarree())
+    axs.coastlines()
+    fig.colorbar(fig1, label=var.name + ' units =' + var.units)
+    axs.set_title('long_name = ' + var.long_name)
+    axs.gridlines(draw_labels=True, dms=True, x_inline=False, y_inline=False)
+    #axs.set_extent([0, 100, 0, 100])
+    plt.show()
+
+breakpoint()
+
+
+
+
+
+
+
+
+
+
+
+
+remote_folder = 'E:/nemo_4.2.2/cfgs/ERA5_input/'
+#filename = remote_folder + 'AQUA_MODIS.20170701.L3m.DAY.PAR.par.4km.nc'
+# Start and end dates in the format YYYYMMDD
+start_date = '20170705'
+end_date = '20170707'
+# Convert the date strings to datetime objects
+current_date = datetime.strptime(start_date, '%Y%m%d')
+end_date = datetime.strptime(end_date, '%Y%m%d')
+while current_date <= end_date:
+    # Format the current date as YYYYMMDD
+    date_str = current_date.strftime('%Y%m%d')
+
+    # Construct the filename
+    filename = f'AQUA_MODIS.{date_str}.L3m.DAY.PAR.par.4km.nc'
+    filepath = remote_folder + filename
+    df = nc.Dataset(filepath)
+    par = np.array(df['par'])
+    par[par<-10000]=np.nan
+    lon = np.array(df['lon'])
+    idxlon = (lon<-30)&(lon>-42)
+    lon = lon[idxlon]
+    lat = np.array(df['lat'])
+    idxlat = (lat<-50)&(lat>-58)
+    lat = lat[idxlat]
+    par1 = par[idxlat,:]
+    par2 = par1[:,idxlon]
+    par2 = (par2*6.02e23)/(86400*2.77e18)
+    fig, axs = plt.subplots(figsize=(12, 8), nrows=1, ncols=1, subplot_kw={'projection': ccrs.PlateCarree()})
+    fig1 = axs.pcolormesh(lon, lat, par2, cmap=plt.get_cmap('hot'), transform=ccrs.PlateCarree())
+    fig.colorbar(fig1, label='W m${^-2}$', shrink=.5)
+    axs.set_title('datetime: ' + df.time_coverage_start)
+    axs.gridlines(draw_labels=True, dms=True, x_inline=False, y_inline=False)
+    axs.coastlines()
+    plt.show()
+    breakpoint()
+
+breakpoint()
 df = nc.Dataset(filename)
 lon = df['lon']
 lat = df['lat']
@@ -64,6 +90,15 @@ for str_v in ['u10','v10', 'd2m','t2m', 'msl', 'sp', 'tp',  'sf','strd','ssrd']:
     #axs.set_extent([0, 100, 0, 100])
     plt.show()
 breakpoint()
+
+
+
+
+
+
+
+
+
 
 
 
